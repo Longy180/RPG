5f640191c1f4828cdd8ec67c65278ce34be20579
root <root@MININT-LR4TVAO>
2023-10-10 12:54:58 +1030

    Here.

--- game.h
+++ game.h
@@ -1,13 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
 #include <SFML/Graphics.hpp>
-
-#include <string>
-#include "player.h"
-#include "enemy.h"
-#include "map.h"
-
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
 #include <iostream>
-
-using namespace sf;
+#include <player.h>
+#include <enemy.h>
+#include <map.h>
 
 class Game
 {
@@ -23,89 +23,7 @@ private:
     Map* map;
 
 public:
-    Game(int size, std::string title, int num_spells, int army_size){
-        win = new sf::RenderWindow(sf::VideoMode(size,size),title);
-        map = new Map(10);
-        player = new Player(10, 250, 250, num_spells);
-        enemy = new Enemy[army_size];
-        this->_army_size = army_size;
-        this->_num_spells = num_spells;
-        _score = 0;
-        if(!font.loadFromFile("Fonts/VideoGame_Font.ttf")){
-            std::cout << "Font not Found\n";
-            exit(0);
-        }
-        info.setFont(font);
-        info.setFillColor(sf::Color::White);
-        info.setCharacterSize(20);
-    }
 
-    void check_score(){
-        for (int i = 0; i < _army_size; i++){
-            if(enemy[i].isAlive()){
-                if(enemy[i].isHit(player->get_x(), player->get_y(), player->get_depth())){
-                    _score--;
-                    enemy[i].die();
-                    enemy[i].spawn(win->getSize().x-10, win->getSize().y);
-                }
-                if(player->isHit(enemy[i].get_x(), enemy[i].get_y(), enemy[i].get_depth())){
-                    _score++;
-                    enemy[i].die();
-                    enemy[i].spawn(win->getSize().x-10, win->getSize().y);
-                }
-            }
-        }
-        
-    }
-
-    void run(){
-        while (win->isOpen())
-        {
-            Event e;
-            while (win->pollEvent(e))
-            {
-                if (e.type == sf::Event::Closed) {
-                    win->close();
-                }
-                if(Keyboard::isKeyPressed(Keyboard::A)){
-                    map->move_left(6);
-                } else if (Keyboard::isKeyPressed(Keyboard::D)){
-                    map->move_right(6);
-                } else if (Keyboard::isKeyPressed(Keyboard::W)){
-                    map->move_up(6);
-                } else if (Keyboard::isKeyPressed(Keyboard::S)){
-                    map->move_down(6);
-                }
-
-                if (Keyboard::isKeyPressed(Keyboard::E)){
-                    for (int i = 0; i < _army_size; i++){
-                        enemy[i].spawn(win->getSize().x-10, win->getSize().y);
-                    }
-                }
-                
-            }
-            win->clear();
-            check_score();
-            if(_score < 0 ){
-                break;
-            }
-            map->draw(win);
-            player->draw(win);
-            for (int i = 0; i < _army_size; i++){
-                enemy[i].draw(win);
-            }
-            std::string msg;
-            msg = "Score: " + std::to_string(_score) + " ";
-            info.setString(msg);
-            win->draw(info);
-            win->display();
-            
-        }
-        
-    }
-    ~Game(){
-        delete this->win;
-        delete this->player;
-        delete [] this->enemy;
-    }
 };
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
 #include <SFML/Graphics.hpp>
 
 #include <string>
-#include "game.h"
 #include "player.h"
 #include "enemy.h"
 #include "map.h"
 
 #include <iostream>
 
-using namespace sf;
+
+
+
 
 int main()
 {
-    Game game(500, "OOP Game", 100, 10);
-    game.run();
+    // sf::RenderWindow window(sf::VideoMode(1300, 732), "Game_Name");
+
+
+    // sf::Texture backgroundTexture;
+    // backgroundTexture.loadFromFile("images/map.png");
+    // sf::Sprite background(backgroundTexture);
+    // background.setPosition(0, 0);
+    // window.draw(background);
+
+    window.display();
     
 }
