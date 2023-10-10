9af45cf3d234a8e19e43c9c0438f1fcad2791110
root <root@MININT-LR4TVAO>
2023-10-10 16:46:58 +1030

    Make file and map working.

--- player.h
+++ player.h
@@ -2,59 +2,29 @@
 #define PLAYER_H
 
 #include <SFML/Graphics.hpp>
+
 #include "spell.h"
 
 class Player {
-    private:
-        sf::CircleShape* body;
-        int _depth;
-    public:
-        Player(int r, int x, int y, int num_O_Spells){
-            body = new sf::CircleShape();
-            body->setRadius(r);
-            body->setPosition(x,y);
-            body->setFillColor(sf::Color::Green);
-            body->setOrigin(r/2,r/2);
-            _depth = r;
-        }
-        void draw(sf::RenderWindow* win){
-            win->draw(*body);
-        }
+ protected:
+  sf::Texture* playerBody;
+  int _depth;
 
-        int get_x(){
-            return body->getPosition().x;
-        }
-        int get_y(){
-            return body->getPosition().y;
-        }
-        int get_depth(){
-            return _depth;
-        }
+ public:
+  Player();
+  void draw(sf::RenderWindow* win);
 
-        
-        void move_right(int speed){
-            body->move(speed,0);
-        }
-        void move_left(int speed){
-            body->move(-speed,0);
-        }
-        void move_up(int speed){
-            body->move(0,-speed);
-        }
-        void move_down(int speed){
-            body->move(0,speed);
-        }
+  int get_x();
+  int get_y();
 
-        bool isHit(int t_x,int t_y, int t_depth){
-            bool hit = false;
+  void move_right(sf::Texture& player, sf::Sprite& playerBody, float distance);
+  void move_left(sf::Texture& player, sf::Sprite& playerBody, float distance);
+  void move_up(sf::Texture& player, sf::Sprite& playerBody, float distance);
+  void move_down(sf::Texture& player, sf::Sprite& playerBody, float distance);
 
-            return hit;
-        }
+  bool isHit(int t_x, int t_y, int t_depth);
 
-
-        ~Player(){
-            delete this->body;
-        }
+  ~Player();
 };
 
 #endif
