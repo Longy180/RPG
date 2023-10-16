f790e5297492f9e2255ff43b379e221737582347
root <root@MININT-LR4TVAO>
2023-10-16 17:52:59 +1030

    Collision working.

--- collision.cpp
+++ collision.cpp
@@ -18,25 +18,12 @@ Collision::Collision() {
   rectangles[1].setPosition(1325, 425);
 }
 
-// To remove barrier between areas, to be added if enough time (would just
-// delete the rectangle that sits in between a door) void
-// Collision::removeCollision(){
-
-// }
-
 // This is for debugging to show where the hitboxes are
 void Collision::drawRectangles(sf::RenderWindow& window) {
   for (int i = 0; i < 10; i++) {
     window.draw(rectangles[i]);
   }
 }
-
-// bool Collision::willHit(Entity& player) {
-//   if ((player.get_x() + 12  || player.get_x() - 12 || player.get_y() + 12 ||
-//        player.get_y() - 12)) {
-
-//   }
-// }
 
 bool Collision::willHit(Entity& player) {
   sf::FloatRect playerBounds = player.get_EntitySprite().getGlobalBounds();
@@ -52,14 +39,6 @@ bool Collision::willHit(Entity& player) {
   return false;  // No collision
 }
 
-// bool Collision::willHitDown(){
-// if player1.get_x()  player1.get_y()
-// }
-
-// bool Collision::willHitLeft(){
-// if player1.get_x()  player1.get_y()
-// }
-
-// bool Collision::willHitRight(){
-// if player1.get_x()  player1.get_y()
-// }
+// To remove barrier between areas, to be added if enough time (would just
+// delete the rectangle that sits in between a door) void
+// Collision::removeCollision(){
--- main.cpp
+++ main.cpp
@@ -9,89 +9,90 @@
 #include "collision.h"
 #include "enemy.h"
 #include "entity.h"
-#include "map.h"
-#include "player.h"
-#include "ranger.h"
-#include "mage.h"
 #include "fighter.h"
 #include "item.h"
+#include "mage.h"
+#include "map.h"
 #include "oldWeapon.h"
-
+#include "player.h"
+#include "ranger.h"
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(1300, 732), "Dog Thief", sf::Style::Close);
-    sf::Text text;
-    sf::Font font;
-    if (!font.loadFromFile("Fonts/VideoGame_Font.ttf")) {
-        std::cout << "error loading font\n";
-    }
-    text.setFont(font);
-    text.setCharacterSize(24);
-    text.setFillColor(sf::Color::White);
-    text.setString("Choose your class:\n1. Fighter\n2. Mage\n3. Ranger");
-    text.setPosition(20.0f, 20.0f);
+  sf::RenderWindow window(sf::VideoMode(1300, 732), "Dog Thief",
+                          sf::Style::Close);
+  sf::Text text;
+  sf::Font font;
+  if (!font.loadFromFile("Fonts/VideoGame_Font.ttf")) {
+    std::cout << "error loading font\n";
+  }
+  text.setFont(font);
+  text.setCharacterSize(24);
+  text.setFillColor(sf::Color::White);
+  text.setString("Choose your class:\n1. Fighter\n2. Mage\n3. Ranger");
+  text.setPosition(20.0f, 20.0f);
 
-    sf::Text combatText;
-    combatText.setFont(font);
-    combatText.setCharacterSize(24);
-    combatText.setFillColor(sf::Color::White);
-    
+  sf::Text combatText;
+  combatText.setFont(font);
+  combatText.setCharacterSize(24);
+  combatText.setFillColor(sf::Color::White);
 
-    int playerClass = 0;
-    bool validChoice = false;
+  int playerClass = 0;
+  bool validChoice = false;
 
-    //Initialise player
-    Player player1((std::string)"Sprites/halfling ranger/HalflingRangerIdleSide.gif", 200, 200, 100, 100, 10, 0);
+  // Initialise player
+  Player player1(
+      (std::string) "Sprites/halfling ranger/HalflingRangerIdleSide.gif", 200,
+      200, 100, 100, 10, 0);
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (event.type == sf::Event::TextEntered) {
-                if (event.text.unicode >= '1' && event.text.unicode <= '3') {
-                    playerClass = event.text.unicode - '0';
-                    validChoice = true;
-                }
-            }
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+      if (event.type == sf::Event::TextEntered) {
+        if (event.text.unicode >= '1' && event.text.unicode <= '3') {
+          playerClass = event.text.unicode - '0';
+          validChoice = true;
         }
-
-        window.clear();
-        window.draw(text);
-        window.display();
-
-
-
-        if (validChoice) {
-            // Based on the player's choice, set the player's class
-            switch (playerClass) {
-                case 1: // Fighter
-                    std::cout << "You chose Fighter." << std::endl;
-                      // Create Player
-                      player1 = Fighter();
-                      combatText.setString("Your Turn:\n1. Tackle\n2. Takedown\n3. Recover");
-                    break;
-                case 2: // Mage
-                    std::cout << "You chose Mage." << std::endl;
-                    // Create Player
-                      player1 = Mage();
-                      combatText.setString("Your Turn:\n1. Cast Fireball\n2. Summon Lightning\n3. Meditate");
-                    break;
-                case 3: // Ranger
-                    std::cout << "You chose Ranger." << std::endl;
-                    // Create Player
-                      player1 = Ranger();
-                      combatText.setString("Your Turn:\n1. Stab\n2. Volley\n3. Survival Skills");
-                    break;
-                default:
-                    // Handle invalid choice
-                    std::cout << "Invalid choice. Please choose 1, 2, or 3." << std::endl;
-            }
-            break;
-        }
+      }
     }
 
+    window.clear();
+    window.draw(text);
+    window.display();
+
+    if (validChoice) {
+      // Based on the player's choice, set the player's class
+      switch (playerClass) {
+        case 1:  // Fighter
+          std::cout << "You chose Fighter." << std::endl;
+          // Create Player
+          player1 = Fighter();
+          combatText.setString(
+              "Your Turn:\n1. Tackle\n2. Takedown\n3. Recover");
+          break;
+        case 2:  // Mage
+          std::cout << "You chose Mage." << std::endl;
+          // Create Player
+          player1 = Mage();
+          combatText.setString(
+              "Your Turn:\n1. Cast Fireball\n2. Summon Lightning\n3. Meditate");
+          break;
+        case 3:  // Ranger
+          std::cout << "You chose Ranger." << std::endl;
+          // Create Player
+          player1 = Ranger();
+          combatText.setString(
+              "Your Turn:\n1. Stab\n2. Volley\n3. Survival Skills");
+          break;
+        default:
+          // Handle invalid choice
+          std::cout << "Invalid choice. Please choose 1, 2, or 3." << std::endl;
+      }
+      break;
+    }
+  }
 
   sf::Clock clock;
   sf::Time elapsedTime;
@@ -119,8 +120,7 @@ int main() {
   // Create Collisions
   Collision mapCollision;
 
-
-  //Initiate player turn and move chosen
+  // Initiate player turn and move chosen
   bool playerTurn = true;
   int moveChosen;
 
@@ -150,104 +150,130 @@ int main() {
           enemyInProximity = &boss1;
         }
 
+        if (enemyInProximity) {
+          inCombat = true;
+          // You can add combat initialization logic here
+          std::cout << "A battle has begun \n";
+          bool playerTurn = true;
+        }
+      } else {
+        // Handle combat logic here
+        playerTurn = true;
+        bool playerHasChosen = false;
 
-            if (enemyInProximity) {
-              inCombat = true;
-            // You can add combat initialization logic here
-            std::cout << "A battle has begun \n";
-            bool playerTurn = true;
-            }
-        } else {
-            // Handle combat logic here
-            playerTurn = true;
-    bool playerHasChosen = false;
-    
-    while (enemyInProximity->get_Health() > 0) {
-        std::cout << "You: \n" << "Health: " << player1.get_Health() << "/" << player1.get_maxHealth();
-        std::cout << "\n Enemy: \n" << "Health: " << enemyInProximity->get_Health() << "/" << enemyInProximity->get_maxHealth() << "\n";
+        while (enemyInProximity->get_Health() > 0) {
+          std::cout << "You: \n"
+                    << "Health: " << player1.get_Health() << "/"
+                    << player1.get_maxHealth();
+          std::cout << "\n Enemy: \n"
+                    << "Health: " << enemyInProximity->get_Health() << "/"
+                    << enemyInProximity->get_maxHealth() << "\n";
 
-        if (playerTurn == true) {
+          if (playerTurn == true) {
             std::cout << "Select attack 1, 2, or 3 (Press 1, 2, or 3 key): ";
             playerHasChosen = false;
 
             sf::Event event;
             while (window.pollEvent(event) && !playerHasChosen) {
-                if (event.type == sf::Event::TextEntered) {
-                    if (event.text.unicode >= '1' && event.text.unicode <= '3') {
-                        int moveChosen = event.text.unicode - '0';
-                        if (moveChosen == 1) {
-                            player1.attack1(enemyInProximity);
-                        } else if (moveChosen == 2) {
-                            player1.attack2(enemyInProximity);
-                        } else if (moveChosen == 3) {
-                            player1.heal();
-                        }
-                        playerTurn = false;
-                        playerHasChosen = true;
-                    }
+              if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode >= '1' && event.text.unicode <= '3') {
+                  int moveChosen = event.text.unicode - '0';
+                  if (moveChosen == 1) {
+                    player1.attack1(enemyInProximity);
+                  } else if (moveChosen == 2) {
+                    player1.attack2(enemyInProximity);
+                  } else if (moveChosen == 3) {
+                    player1.heal();
+                  }
+                  playerTurn = false;
+                  playerHasChosen = true;
                 }
+              }
             }
-          
-        } else {
+
+          } else {
             // Handle enemy's attack here, if applicable
             player1.takeDamage(enemyInProximity->get_damage());
             playerTurn = true;
+          }
         }
-    }
 
-            enemyInProximity->die();
-            enemyInProximity = nullptr;
-            inCombat = false;
-            std::cout << "You won! \n";
-            OldWeapon oldBow;
+        enemyInProximity->die();
+        enemyInProximity = nullptr;
+        inCombat = false;
+        std::cout << "You won! \n";
+        OldWeapon oldBow;
 
-            player1.addToInventory(&oldBow);
+        player1.addToInventory(&oldBow);
 
-            Item* retrievedItem = player1.getInventoryItem(0);
+        Item* retrievedItem = player1.getInventoryItem(0);
 
-            if (retrievedItem) {
-                // The item exists in the inventory, you can access its properties.
-                int sellPrice = retrievedItem->get_sellPrice();
-                std::cout << "Item Sell Price: " << sellPrice << std::endl;
-            } else {
-                std::cout << "Item not found in inventory." << std::endl;
-            }
+        if (retrievedItem) {
+          // The item exists in the inventory, you can access its properties.
+          int sellPrice = retrievedItem->get_sellPrice();
+          std::cout << "Item Sell Price: " << sellPrice << std::endl;
+        } else {
+          std::cout << "Item not found in inventory." << std::endl;
         }
-std::cout << "THE CODE EXITS THE FOR STATEMENT" << std::endl;
+      }
+      // std::cout << "THE CODE EXITS THE FOR STATEMENT" << std::endl;
 
       if (!inCombat) {
         elapsedTime = clock.getElapsedTime();
         if (elapsedTime.asSeconds() > 0.1) {
+
+          // Attempting to move LEFT
           if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) ||
               sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
             if (mapCollision.willHit(player1) == false) {
               player1.move_left(12);
               std::cout << "Left" << std::endl;
             }
+            if (mapCollision.willHit(player1) == true) {
+              player1.move_right(12);
+              std::cout << "Moving right from collision" << std::endl;
+            }
             clock.restart();
+
+            // Attempting to move RIGHT
           } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) ||
                      sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
             if (mapCollision.willHit(player1) == false) {
               player1.move_right(12);
               std::cout << "Right" << std::endl;
             }
+            if (mapCollision.willHit(player1) == true) {
+              player1.move_left(12);
+              std::cout << "Moving left from collision" << std::endl;
+            }
             clock.restart();
+
+            // Attempting to move UP
           } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) ||
                      sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
             if (mapCollision.willHit(player1) == false) {
               player1.move_up(12);
               std::cout << "Up" << std::endl;
             }
+            if (mapCollision.willHit(player1) == true) {
+              player1.move_down(12);
+              std::cout << "Moving down from collision" << std::endl;
+            }
             clock.restart();
+
+            // Attempting to move DOWN
           } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) ||
                      sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
             if (mapCollision.willHit(player1) == false) {
               player1.move_down(12);
               std::cout << "Down" << std::endl;
             }
+            if (mapCollision.willHit(player1) == true) {
+              player1.move_up(12);
+              std::cout << "Moving up from collision" << std::endl;
+            }
             clock.restart();
           }
-          // Closes the map until pressed twice
         }
       }
     }
@@ -288,7 +314,8 @@ std::cout << "THE CODE EXITS THE FOR STATEMENT" << std::endl;
           view.getCenter().y + 90);
       combatTextBox.setScale(0.5, 0.5);
       window.draw(combatTextBox);
-      combatText.setPosition(window.getView().getCenter().x - 200, window.getView().getCenter().y + 110);
+      combatText.setPosition(window.getView().getCenter().x - 200,
+                             window.getView().getCenter().y + 110);
       window.draw(combatText);
     }
     window.display();
