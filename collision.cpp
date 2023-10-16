9c1d7f08966af043ef9efa36893f5c38d221e4f0
Rory <rory.long18@gmail.com>
2023-10-16 19:19:37 +1030

    coll

--- collision.cpp
+++ collision.cpp
@@ -13,35 +13,40 @@
 // Constructor that creates all border collisions
 Collision::Collision() {
   rectangles[0].setSize(sf::Vector2f(1275, 25));  // Set size of rectangle
-  rectangles[0].setPosition(0, 425);              // Set position as needed
+  rectangles[0].setPosition(0, 425);              // Set position of the rectangle
   rectangles[1].setSize(sf::Vector2f(100, 25));
   rectangles[1].setPosition(1325, 425);
+  rectangles[2].setSize(sf::Vector2f(25, 400));
+  rectangles[2].setPosition(0, 0);
+  rectangles[3].setSize(sf::Vector2f(90, 25));
+  rectangles[3].setPosition(0, 150);
+  rectangles[4].setSize(sf::Vector2f(250, 25));
+  rectangles[4].setPosition(190, 150);
+  rectangles[5].setSize(sf::Vector2f(25, 70));
+  rectangles[5].setPosition(275, 170);
+  rectangles[6].setSize(sf::Vector2f(450, 30));
+  rectangles[6].setPosition(0, 0);
+  rectangles[7].setSize(sf::Vector2f(25, 300));
+  rectangles[7].setPosition(415, 0);
+  rectangles[8].setSize(sf::Vector2f(175, 25));
+  rectangles[8].setPosition(425, 270);
+  rectangles[9].setSize(sf::Vector2f(280, 25));
+  rectangles[9].setPosition(680, 270);
+  rectangles[10].setSize(sf::Vector2f(40, 150));
+  rectangles[10].setPosition(960, 150);
 }
-
-// To remove barrier between areas, to be added if enough time (would just
-// delete the rectangle that sits in between a door) void
-// Collision::removeCollision(){
-
-// }
 
 // This is for debugging to show where the hitboxes are
 void Collision::drawRectangles(sf::RenderWindow& window) {
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 20; i++) {
     window.draw(rectangles[i]);
   }
 }
 
-// bool Collision::willHit(Entity& player) {
-//   if ((player.get_x() + 12  || player.get_x() - 12 || player.get_y() + 12 ||
-//        player.get_y() - 12)) {
-
-//   }
-// }
-
 bool Collision::willHit(Entity& player) {
   sf::FloatRect playerBounds = player.get_EntitySprite().getGlobalBounds();
 
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < 20; i++) {
     if (playerBounds.intersects(rectangles[i].getGlobalBounds())) {
       // There's a collision
       std::cout << "collision" << std::endl;
@@ -52,14 +57,6 @@ bool Collision::willHit(Entity& player) {
   return false;  // No collision
 }
 
-// bool Collision::willHitDown(){
-// if player1.get_x()  player1.get_y()
-// }
-
-// bool Collision::willHitLeft(){
-// if player1.get_x()  player1.get_y()
-// }
-
-// bool Collision::willHitRight(){
-// if player1.get_x()  player1.get_y()
-// }
+// To remove barrier between areas, to be added if enough time (would just
+// delete the rectangle that sits in between a door) void
+// Collision::removeCollision(){
